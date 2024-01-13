fb89e70f6e7d42c993c5807adf8107abd0f0ee96
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-13 14:21:53 +0100

    Loading all places from the frontend rather from the backend

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -18,12 +18,42 @@ $( document ).ready(() => {
 	if (data.status === 'OK') {
 	    $('div#api_status').addClass('available');
 
+	    function OrderListBy(prop) {
+		return function (a, b) {
+		    if (a[prop] > b[prop]) {
+			return 1;
+		    }
+		    else if (a[prop] < b[prop]) {
+			return -1;
+		    }
+		    return 0;
+		}
+	    }
+
 	    $.post({
 		url: 'http://0.0.0.0:5001/api/v1/places_search',
 		contentType: 'application/json',
 		data: JSON.stringify({}),
 		success: function (response) {
-		    console.log(response);
+		    for (let place of response.sort(OrderListBy('name'))) {
+			console.log(place);
+			placeHTML = `<article>
+	                  <div class="title_box">
+	                   <h2>${place.name}</h2>
+	                   <div class="price_by_night">$${place.price_by_night}</div>
+	                  </div>
+                	  <div class="information">
+	                   <div class="max_guest">${place.max_guest} Guests</div>
+                           <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                           <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                	  </div>
+                          <div class="description">
+	                    ${place.description}
+                          </div>
+                	</article>`
+
+			$('section.places').append(placeHTML);
+		    }
 		},
 		error: function (error) {
 		    console.log(error);
